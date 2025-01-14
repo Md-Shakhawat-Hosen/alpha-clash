eb251ffa5390d7f7630a02a0b59d53de918e3aaa
Md-Shakhawat-Hosen <shakhawat.hosen.nsu@gmail.com>
2025-01-14 22:47:28 +0600

    added utility js file for reuse function

--- scripts/crash.js
+++ scripts/crash.js
@@ -1,9 +1,7 @@
 function play(){
-    const home = document.getElementById('home');
-    const play_ground = document.getElementById("play-ground");
-    console.log(play_ground);
-    console.log(home);
-
+    const home = getElementId('home')
+    const play_ground = getElementId('play-ground');
+   
     home.classList.add('hidden');
     play_ground.classList.remove('hidden');
 }
---
+++ scripts/utility.js
@@ -0,0 +1,5 @@
+function getElementId(fieldId){
+    const element = document.getElementById(fieldId);
+
+    return element;
+}
